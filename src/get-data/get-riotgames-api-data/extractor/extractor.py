bbfa302bae3ea0f1023115685e41adccdc80eedd
valentin fontanger <valentin.fontanger@gmail.com>
2022-03-30 14:22:28 +0200

    extractions ok

--- src/get-data/get-riotgames-api-data/extractor/extractor.py
+++ src/get-data/get-riotgames-api-data/extractor/extractor.py
@@ -1,8 +1,30 @@
 from urllib import response
 import requests
 import json
-from decorator import request # custom decorator
-from constant import (PUUIS_ENDPOINT, HEADERS, MATCHS_ENDPOINTS, MATCH_DETAIL_ENDPOINTS,DIVISION_ENDPOINT) # endpoints
+from decorator import request
+from soupsieve import match # custom decorator
+from constant import (
+    PUUID_ENDPOINT, 
+    MATCH_ENDPOINT, 
+    MATCH_DETAIL_ENDPOINTS, 
+    MATCH_DETAIL_TIMELINE, 
+    HEADERS, 
+    RIOT_SERVER, 
+    REGION_ENDPOINT
+    ) # endpoints
+
+
+def get_server(server):
+
+    if server.lower() not in RIOT_SERVER: 
+        raise ValueError(f"{server} is unkown. Please, select one of the following servers : ", RIOT_SERVER.keys())
+    return RIOT_SERVER[server.lower()]
+
+
+def get_region(region): 
+    if region.lower() not in REGION_ENDPOINT: 
+        raise ValueError(f"{region} is unkown. Please, select one of the following regions : ", REGION_ENDPOINT.keys())
+    return REGION_ENDPOINT[region.lower()]
 
 
 class UserDataExtractor: 
@@ -11,12 +33,14 @@ class UserDataExtractor:
         A class responsible for extracting user related info. 
     """
     @request
-    def get_puuid_by_account_id(self, account_id): 
+    def get_user_info(self, account_id, server="EUW1"): 
         """
-            retrieve a user's puuid
+            retrieve user informations and the endpoint in order to be consumed
         """
-        endpoint = PUUIS_ENDPOINT + account_id
-        puuid = requests.get(endpoint, headers=HEADERS)["puuid"]
+        server = get_server(server)
+        endpoint = server + PUUID_ENDPOINT + account_id
+        puuid = requests.get(endpoint, headers=HEADERS)
+        print(puuid)
         return {
             "response" : puuid, 
             "endpoint" : endpoint
@@ -24,17 +48,17 @@ class UserDataExtractor:
 
 
     @request
-    def retrieve_matches(self, puuid): 
+    def get_matches(self, puuid, region="europe"): 
         """
-            Returns a list of game'id
+            Returns a list of game'id and the endpoint in order to be consumed
         """
-        endpoint = MATCHS_ENDPOINTS + f"{puuid}/ids?type=ranked&start=0&count=25"
+        region = get_region(region)
+        endpoint = region + MATCH_ENDPOINT + f"{puuid}/ids?type=ranked&start=0&count=25"
         r = requests.get(endpoint, headers=HEADERS)
         return {
             "response" : r, 
             "endpoint" : endpoint
         }
-
 
 
 
@@ -44,15 +68,27 @@ class MatchDataExtractor:
     """
     
     @request
-    def retrieve_match_content(self, match_id): 
+    def retrieve_match_content(self, match_id, region="europe"): 
         """
-            Returns a game's content by its id
+            Returns a game's content and the endpoint in order to be consumed
         """
-        endpoint = MATCH_DETAIL_ENDPOINTS + match_id
+        region = get_region(region)
+        endpoint = region + MATCH_DETAIL_ENDPOINTS + match_id
         r = requests.get(endpoint, headers=HEADERS)
-        
+
         return {
             "response" : r, 
             "endpoint" : endpoint
         }
 
+
+
+# extractor = UserDataExtractor() 
+# d_puuid = extractor.get_user_info("eK-5G4INu5yvmAbQmxHkS3gSdan6EUCzfZ9dS0EbJCmlawM")
+# puuid = d_puuid["puuid"]
+# matches = extractor.get_matches(puuid)
+# print(matches)
+
+# match_extractor = MatchDataExtractor()
+# content = match_extractor.retrieve_match_content(matches[0])
+# print(content)
